acc629e2bfba5b5744f3a4b2999c5ccf696e1161
Sergii Savchenko <sergii.savchenko@gmail.com>
2015-11-28 01:48:17 +0200

    Fix not found

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'sergiisavchenko:jointjs',
-  version: '0.9.5_1',
-  summary: 'Meteor Package for JointJS 0.9.5',
+  version: '0.9.5_6',
+  summary: 'Meteor Package for JointJS 0.9.5. could\'t fix dependencies. Don\'t use',
   git: 'https://github.com/sergii-savchenko/meteor-jointjs.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('karfield:backbone@1.2.1', 'client');
   api.use('erasaur:meteor-lodash@3.10.1_1', 'client');
-  api.addFiles([ 'joint.min.js', 'joint.min.css' ], 'client');
+  api.use('backbone@1.0.0', 'client');
+  api.addFiles([ 'jointjs.js', 'jointjs.css' ], 'client');
 
 });
 
